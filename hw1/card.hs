7090ee26d2c1461b7ca58f5507703d88478c2701
James Tien <southp0105@gmail.com>
2014-09-03 13:58:45 +0800

    some coding style fixes

--- hw1/card.hs
+++ hw1/card.hs
@@ -7,18 +7,17 @@ dropLastDigit n = n `div` 10
 toInverseDigits :: Integer -> [Integer]
 toInverseDigits n
     | n <= 0    = []
-    | otherwise = lastDigit n : toInverseDigits(dropLastDigit n)
+    | otherwise = lastDigit n : toInverseDigits (dropLastDigit n)
 
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toInverseDigits
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther dl = reverse(double_worker rl)
+doubleEveryOther dl = reverse (double_worker (reverse dl))
     where
-        rl = reverse dl
         double_worker [] = []
         double_worker [x] = [x]
-        double_worker (r1:r2:rs) = r1:2*r2:double_worker(rs)
+        double_worker (r1:r2:rs) = r1:2*r2:double_worker rs
 
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
