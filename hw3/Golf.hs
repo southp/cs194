beaf68e2e95336fb191cdfac8cbe5e5d40a799e7
James Tien <southp0105@gmail.com>
2014-09-10 10:42:55 +0800

    suggestions from lpaste

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,6 +1,6 @@
 module Golf where
 
-pick_n x n = do_pick x n
+pickN x n = do_pick x n
     where
         do_pick [] _      = []
         do_pick (x:xs) i
@@ -12,7 +12,7 @@ skips x = do_skips 1
     where
         do_skips n
             | n > length x = []
-            | otherwise    = pick_n x n : do_skips (n+1)
+            | otherwise    = pickN x n : do_skips (n+1)
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima (x1:x2:x3:xs)
@@ -23,21 +23,21 @@ localMaxima (x1:x2:x3:xs)
         d2 = x2 - x3
 localMaxima _             = []
 
-count_9 x = [(count 0), (count 1), (count 2), (count 3), (count 4), (count 5), (count 6), (count 7), (count 8), (count 9)]
+count9 x = [(count i) | i <- [0..9]]
     where
-        count n = length (filter (\x' -> x' == n) x)
+        count n = length (filter (== n) x)
 
-histo_n x n = do_histo_n (count_9 x)
+histoN x n = do_histoN (count9 x)
     where
-        do_histo_n []     = "\n"
-        do_histo_n (c:cs)
-            | c >= n      = '*' : do_histo_n cs
-            | otherwise   = ' ' : do_histo_n cs
+        do_histoN []     = "\n"
+        do_histoN (c:cs)
+            | c >= n      = '*' : do_histoN cs
+            | otherwise   = ' ' : do_histoN cs
 
 histogram :: [Integer] -> String
 histogram x = histo_all (maximum counts) ++ "==========\n0123456789\n"
     where
-        counts     = count_9 x
+        counts          = count9 x
         histo_all n
             | n == 0    = ""
-            | otherwise = histo_n x n ++ histo_all (n-1)
+            | otherwise = histoN x n ++ histo_all (n-1)
