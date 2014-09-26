7a0b73575493b143bc94f535120712c6ad8a8b87
James Tien <southp0105@gmail.com>
2014-09-26 11:32:27 +0800

    hw5 prog.

--- hw5/hw5.hs
+++ hw5/hw5.hs
@@ -1,6 +1,13 @@
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
+
+evalStr :: String -> Maybe Integer
+evalStr exp = do_eval (parseExp Lit Add Mul exp)
+    where
+        do_eval Nothing  = Nothing
+        do_eval (Just e) = Just (eval e)
