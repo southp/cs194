75cfc2724c7da2dcc53a13b68789355a762e4c4c
James Tien <southp0105@gmail.com>
2014-10-08 19:15:57 +0800

    StackCalc.hs

---
+++ hw5/StackCalc.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances    #-}
+
+import Parser
+import StackVM
+
+class Expr a where
+    lit :: Integer -> a
+    mul :: a -> a -> a
+    add :: a -> a -> a
+
+instance Expr Program where
+    lit x    = [PushI x]
+    mul x y  = x ++ y ++ [Mul]
+    add x y  = x ++ y ++ [Add]
+
+compile :: String -> Maybe Program
+compile xs = parseExp lit add mul xs :: Maybe Program
