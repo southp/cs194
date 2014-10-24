207bfa0f89cdfd7379cf124b63506d90a4ea64c3
James Tien <southp0105@gmail.com>
2014-10-24 17:01:56 +0800

    2014-10-24

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -5,3 +7,25 @@ fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+-- fibs2 :: [Integer]
+-- fibs2 =
+
+data Stream a = Cons a (Stream a)
+                -- deriving Show
+
+streamToList :: Stream a -> [a]
+streamToList (Cons s ss) = s : streamToList ss
+
+instance Show a => Show (Stream a) where
+    show s = show $ take 20 (streamToList s)
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+-- Functor?
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons s ss) = Cons (f s) (streamMap f ss)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
