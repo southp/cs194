1bb003c8a0528893e2f523c3dbe85228f549f50c
James Tien <southp0105@gmail.com>
2014-10-28 10:56:53 +0800

    2014-10-28

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+
 import Data.List
 
 fib :: Integer -> Integer
@@ -8,11 +11,18 @@ fib n = fib (n-1) + fib (n-2)
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
--- fibs2 :: [Integer]
--- fibs2 =
+-- courtesy of https://www.haskell.org/tutorial/patterns.html
+-- I didn't come up with a viable solution myself :(
+fibs2 :: [Integer]
+fibs2@(0:tfib) = 0 : 1 : zipWith (+) fibs2 tfib
+
+-- courtesy of Haskell wiki http://www.haskell.org/haskellwiki/The_Fibonacci_sequence
+-- I didn't know unfoldr when I did this homework. Cite it here for remembering.
+-- Using tuple here was just so brilliant. Map + iterate + tuple impl. was also creative.
+fibs2' :: [Integer]
+fibs2' = unfoldr (\(a,b) -> Just (a, (b, a+b))) (0,1)
 
 data Stream a = Cons a (Stream a)
-                -- deriving Show
 
 streamToList :: Stream a -> [a]
 streamToList (Cons s ss) = s : streamToList ss
@@ -42,4 +52,17 @@ mod2List x = map (x `mod`) (iterate (*2) 2)
 
 ruler :: Stream Integer
 ruler = streamMap maxPowerOf2 (streamFromSeed (+1) 1)
+
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+intMul :: Integer -> Stream Integer -> Stream Integer
+a `intMul` (Cons b bs) = Cons (a*b) (a `intMul` bs)
+
+instance Num (Stream Integer) where
+    fromInteger n = Cons n (streamRepeat 0)
+
+    (Cons a as) + (Cons b bs) = Cons (a + b) (as + bs)
+
+    (Cons a as) * (Cons b bs) = (fromInteger (a*b)) + (b `intMul` as*x) + (a `intMul` bs*x) + (as*bs*x*x)
 
