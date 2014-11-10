9f16333228d03c4a32f791d44b2167f939937284
James Tien <southp0105@gmail.com>
2014-11-10 18:16:16 +0800

    hw7

--- hw7/JoinListBufEditor.hs
+++ hw7/JoinListBufEditor.hs
@@ -3,11 +3,11 @@ module Main where
 import JoinListBuffer
 import Editor
 
-initialContent = map JB.fromString
+initialContent = map fromString
          [ "This buffer is for notes you don't want to save, and for"
          , "evaluation of steam valve coefficients."
          , "To load a different file, type the character L followed"
          , "by the name of the file."
-         ] -- :: [JoinList (Score, Size) String]
+         ] :: [JoinList (Score, Size) String]
 
 main = runEditor editor $ foldr (+++) Empty initialContent
