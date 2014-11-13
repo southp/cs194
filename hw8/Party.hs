c63931d0dcfe978fd219e4fd533ede15e06e4583
James Tien <southp0105@gmail.com>
2014-11-13 13:33:13 +0800

    hw8 treeFold

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -1,9 +1,11 @@
 module Party where
 
 import Data.Monoid
+import Data.Tree
 
 import Employee
 
+-- ex.1
 glCons :: Employee -> GuestList -> GuestList
 glCons e (GL emps fun) = GL (e:emps) (fun + empFun e)
 
@@ -13,3 +15,8 @@ moreFun gl1 gl2 = if gl1 > gl2 then gl1 else gl2
 instance Monoid GuestList where
     mempty = GL [] 0
     mappend (GL e1 f1) (GL e2 f2) = GL (e1 ++ e2) (f1 + f2)
+
+-- ex.2
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold f b (Node a []) = f a b
+treeFold f b (Node a ts) = f a (foldr (\t ba -> treeFold f ba t) b ts)
