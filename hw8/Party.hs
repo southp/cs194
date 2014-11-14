e225e851ecb574cf2c252fc5e60b5c13d77cfe3b
James Tien <southp0105@gmail.com>
2014-11-14 14:29:21 +0800

    hw8 ex4

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -17,6 +17,19 @@ instance Monoid GuestList where
     mappend (GL e1 f1) (GL e2 f2) = GL (e1 ++ e2) (f1 + f2)
 
 -- ex.2
-treeFold :: (a -> b -> b) -> b -> Tree a -> b
-treeFold f b (Node a []) = f a b
-treeFold f b (Node a ts) = f a (foldr (\t ba -> treeFold f ba t) b ts)
+treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
+treeFold f b (Node a []) = f a [b]
+treeFold f b (Node a ts) = f a (map (treeFold f b) ts)
+
+-- ex.3
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e [] = (GL [e] (empFun e), GL [] 0)
+nextLevel e glps = (boss, noboss)
+    where
+        boss = glCons e (mconcat . map snd $ glps)
+        noboss = mconcat [max gl1 gl2 | (gl1, gl2) <- glps]
+
+-- ex.4
+maxFun :: Tree Employee -> GuestList
+maxFun t = max gl1 gl2
+    where (gl1, gl2) = treeFold nextLevel (GL [] 0, GL [] 0) t
