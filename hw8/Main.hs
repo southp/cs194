6717a606947390af197a5a6a4db0938afce49d26
James Tien <southp0105@gmail.com>
2014-11-14 16:48:01 +0800

    fin. hw8

---
+++ hw8/Main.hs
@@ -0,0 +1,17 @@
+import Data.Tree
+import Data.List
+
+import System.Environment
+
+import Employee
+import Party
+
+guestListString :: GuestList -> String
+guestListString (GL es f) = "Total fun: " ++ show f ++ "\n" ++ estrs
+    where estrs = concat $ sort . map ((++ "\n") . empName) $ es
+
+main :: IO ()
+main = getArgs >>= (\args -> case args of [] -> putStrLn "Please enter a input name."
+                                          [a] -> readFile a >>= (\fcontent -> return (maxFun (read fcontent)))
+                                                            >>= (\gl -> putStr . guestListString $ gl)
+                                          otherwise -> putStrLn "I only work for one company at a time. You know...")
